333c71e2146d807cd98e6d5836fdd984db68f2c2
Ihor <i.postnikov97@gmail.com>
2023-08-04 02:54:12 +0300

    upd

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    github = {
+      source = "hashicorp/github"
+      version = "4.3.2"
+    }
+  }
+}
+
+
 provider "github" {
   token        = "${var.PAT}"
+  organization = "${var.github_organization}"
 }
+
+# Add a collaborator to a repository
+resource "github_repository_collaborator" "a_repo_collaborator" {
+  repository = "github-terraform-task-ipostnikov"
+  username   = "softservedata"
+  permission = "push"
+}
+
+
