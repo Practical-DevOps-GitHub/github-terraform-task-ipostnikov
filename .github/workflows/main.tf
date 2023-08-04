9a28730766dbaf63537b9217a237f4ecd9eaa887
Ihor Postnikov <Ihor.Postnikov@3shape.com>
2023-08-04 15:48:42 +0300

    test

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -39,13 +39,16 @@ resource "github_branch_protection" "main_protection" {
   
 }
 
+
 resource "github_branch_protection" "develop_protection" {
 
   repository = var.repo_name
   branch     = "develop"
   enforce_admins = true
-  
+
   required_pull_request_reviews {
+
     required_approving_review_count  = 2
+
   }
 }
