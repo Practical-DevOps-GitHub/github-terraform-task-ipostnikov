cccfe4d2c08e5a29aa37e30b2777793d1f6a72cf
Ihor Postnikov <Ihor.Postnikov@3shape.com>
2023-08-04 15:37:26 +0300

    test

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -26,3 +26,26 @@ resource "github_branch_default" "default_branch" {
   repository = var.repo_name
   branch = "develop"
 }
+
+resource "github_branch_protection" "main_protection" {
+  repository = var.repo_name
+  branch     = "main"
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews            = true
+    require_code_owner_reviews       = true
+  }
+
+  enforce_admins = true
+}
+
+resource "github_branch_protection" "develop_protection" {
+
+  repository = var.repo_name
+  branch     = "develop"
+  
+  required_pull_request_reviews {
+    required_approving_review_count  = 2
+  }
+  enforce_admins = true
+}
