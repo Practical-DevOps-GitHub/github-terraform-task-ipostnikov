49afedc73d3bfc0e26fbb07ff3eba72476c69942
Ihor Postnikov <Ihor.Postnikov@3shape.com>
2023-08-04 15:50:33 +0300

    test

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+variable "repo_name" {
+  default = "github-terraform-task-ipostnikov"
+}
+
+variable "org_name" {
+  default = "Practical-DevOps-GitHub"
+}
+
 
 provider "github" {
   token = var.PAT
