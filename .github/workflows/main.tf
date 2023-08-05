1ce404e09861a1a8bccc86b2982a022dfce4a082
ihpst <i.postnikov97@gmail.com>
2023-08-05 09:19:41 +0000

    updates

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -8,17 +8,6 @@ terraform {
   }
 }
 
-variable "repo_name" {
-  type    = string
-  default = "github-terraform-task-ipostnikov"
-}
-
-
-variable "org_name" {
-  default = "Practical-DevOps-GitHub"
-}
-
-
 provider "github" {
   token = var.PAT
   owner = "Practical-DevOps-GitHub"
@@ -44,7 +33,7 @@ resource "github_branch_default" "defbranch" {
 
 resource "github_branch_protection" "develop_protection" {
 
-  repository_id = data.github_repository.repo_name.id
+  repository_id = data.github_repository.repo_name_id.id
   pattern       = "develop"
   required_pull_request_reviews {
     required_approving_review_count = 2
@@ -53,7 +42,7 @@ resource "github_branch_protection" "develop_protection" {
 
 resource "github_branch_protection" "main_protection" {
   #main branch protection rule
-  repository_id = data.github_repository.repo_name.id
+  repository_id = data.github_repository.repo_name_id.id
   pattern       = "main"
 
   required_pull_request_reviews {
@@ -64,15 +53,15 @@ resource "github_branch_protection" "main_protection" {
 
 resource "github_repository_file" "soft_codeowner" {
   repository = var.repo_name
-  branch = "develop"
-  content = "* @softservedata"
-  file = "CODEOWNERS"
+  branch     = "develop"
+  content    = "* @softservedata"
+  file       = "CODEOWNERS"
 }
 
 resource "github_repository_file" "pull_request_file" {
   repository = var.repo_name
-  file = ".github/pull_request_template.md"
-  content = <<EOF
+  file       = ".github/pull_request_template.md"
+  content    = <<EOF
       ## Describe your changes
 
     ## Issue ticket number and link
@@ -94,18 +83,18 @@ resource "github_repository_deploy_key" "deploy_key" {
 
 resource "github_repository_webhook" "discord_webhook" {
   repository = var.repo_name
-  events = ["pull_request"]
-  
+  events     = ["pull_request"]
+
   configuration {
-    url = "https://discord.com/api/webhooks/1137122289633198110/MtNHA9ieOGVeg0oHuvlBMtnfJuBi-iNGpqpkxZxO_u4IJ9XSHRFClerRZlOxdS70JU6o/github"
+    url          = "https://discord.com/api/webhooks/1137122289633198110/MtNHA9ieOGVeg0oHuvlBMtnfJuBi-iNGpqpkxZxO_u4IJ9XSHRFClerRZlOxdS70JU6o/github"
     content_type = "form"
 
   }
 }
 
 resource "github_actions_secret" "PAT" {
-  repository = var.repo_name
-  secret_name = "PAT"
+  repository      = var.repo_name
+  secret_name     = "PAT"
   plaintext_value = var.PAT
 }
 
--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,13 +1,21 @@
 
-#vars
 
-# variable "repo_name" {
-#   type    = string
-#   default = "github-terraform-task-ipostnikov"
-# }
+variable "repo_name" {
+  type    = string
+  default = "github-terraform-task-ipostnikov"
+}
 
+variable "PAT" {
+  type        = string
+  description = "Git Hub Personal Access Token"
+}
 
-# variable "org_name" {
-#   default = "Practical-DevOps-GitHub"
-# }
+#get repo id
 
+data "github_repository" "repo_name_id" {
+  full_name = "Practical-DevOps-GitHub/github-terraform-task"
+}
+
+output "repository_id" {
+  value = data.github_repository.repo_name_id.id
+}
